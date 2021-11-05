1fcfff4cc32e233bd32421d8ac463b37031d4261
Gresba <79733876+Gresba@users.noreply.github.com>
2021-11-04 21:28:05 -0400

    Update DoublyLinkedListDemoApp.java

--- src/main/java/Week10/DoublyLinkedListDemoApp.java
+++ src/main/java/Week10/DoublyLinkedListDemoApp.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Slight edited version of https://github.com/moaathalrajab/DoublyLinkedList_Stack_Queue_Demo/blob/master/src/main/java/com/mycompany/dllists_stacks_queues/DoublyLinkedListDemoApp.java
 package Week10;
 
 /**
